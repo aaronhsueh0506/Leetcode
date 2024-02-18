68e847bd5c214e1318d68124b94dba6cde631fe1
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-18 22:14:11 +0100

    Create sol-medium.cpp

---
+++ 207. Course Schedule/sol-medium.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+      vector<int> visited(numCourses);
+      vector<vector<int>> graph(numCourses, vector<int>());
+
+      for(auto a: prerequisites){
+        graph[a[1]].push_back(a[0]);
+      }
+
+      for(int i=0;i<numCourses;i++){
+        if(!DFS(graph, visited, i)) return false;
+      }
+      return true;
+    }
+
+    bool DFS(vector<vector<int>> &graph, vector<int> &visited, int i){
+      if(visited[i]==1) return true;
+      if(visited[i]==-1) return false;
+      visited[i] = -1;
+      for(auto a: graph[i]){
+        if(!DFS(graph, visited, a)) return false;
+      }
+      visited[i]=1;
+      return true;
+    }
+}
