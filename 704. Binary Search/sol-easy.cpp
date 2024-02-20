0d762ee2bcb89473cd26d5296c23000f787fcf82
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-20 10:15:17 +0100

    Create sol-easy.cpp

---
+++ 704. Binary Search/sol-easy.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      int left=0, right=nums.size();
+
+      while(left<right){
+        int mid = left + (right-left)/2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] > target) right = mid;
+        else left = mid+1;
+      }
+      return -1;
+    }
+};
