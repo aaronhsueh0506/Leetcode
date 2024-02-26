6016fd508c7722f506c2074c3d9557b0df893b5c
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-26 13:32:01 +0800

    Create sol-medium.cpp

---
+++ 692. Top K Frequent Words/sol-medium.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+      unordered_map<string, int> m;
+      for(string word:words) m[word]++;
+
+      priority_queue<pair<strint, int>, vector<pair<string,int>>, comp> q;
+      for(auto w:m){
+        q.push(w);
+      }
+
+      vector<string> res;
+      for(int i=0;i<k;i++){
+        res.push_back(q.top().first);
+        q.pop();
+      }
+      return res;
+    }
+
+    struct comp{
+      bool operater(){ pair<strint, int>& l1, pair<strint, int>& l2){
+        if(l1.second == l2.second) return l1.first > l2.first;
+        return l1.second < l2.second;
+      }
+    }
+};
