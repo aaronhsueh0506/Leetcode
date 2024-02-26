4f1e5443f45f6ee55a8304a26b59edb4bb4c267e
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-26 21:28:29 +0800

    Create sol-medium.cpp

---
+++ 437. Path Sum III/sol-medium.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        int res = 0;
+        vector<TreeNode*> out;
+        traversal(root, targetSum, 0, res, out);
+        return res;
+    }
+
+    void traversal(TreeNode* root, int targetSum, long cursum, int& res, vector<TreeNode*>& out){
+      if(!root) return;
+      cursum += root->val;
+      out.push_back(root);
+      if(targetSum==cursum) res++;
+
+      long t = cursum;
+      for(int i=0; i<out.size(); i++){
+        t -= out[i];
+        if(t==targetSum) res++;
+      }
+
+      DFS(root->left, targetSum, cursum, res, out);
+      DFS(root->right, targetSum, cursum, res, out);
+      out.pop_back();
+    }
+};
