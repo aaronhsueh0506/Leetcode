abf59a27b2a34bbe8c2f38b585d3ac89b334a923
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-26 21:51:54 +0800

    Create sol-medium.cpp

---
+++ 735. Asteroid Collision/sol-medium.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> res;
+
+        for(int i=0; i<asteroids.size(); i++){
+          if(asteroids[i]>0) res.push_back(asteroids[i]);
+          else if(res.empty() || res.back() < 0) res.push_back(asteroids[i]);
+          else if(res.back()<=-asteroids[i]){
+            if(res.back() < -asteroids[i]) i--;
+            res.pop_back();
+          }
+        }
+      return res;
+    }
+};
