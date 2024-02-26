b15324ab172a25032a0f253fe19a2e07327891ae
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-26 16:47:47 +0800

    Create sol-medium.cpp

---
+++ 525. Contiguous Array/sol-medium.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+      int res = 0, sum = 0;
+      unordered_map<int, int> m{{0, -1}}; //{sum, index}
+      
+      for(int i=0; i<nums.size(); i++){
+        sum += nums[i];
+        if(m.count(sum)) res=max(res, i - m[sum]);
+        else m[sum] = i;
+      }
+      return res;
+    }
+};
