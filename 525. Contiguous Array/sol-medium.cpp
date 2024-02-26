0d3042c68116e2add87f923fdcd106890e5918e2
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-26 16:48:37 +0800

    Update sol-medium.cpp

--- 525. Contiguous Array/sol-medium.cpp
+++ 525. Contiguous Array/sol-medium.cpp
@@ -5,7 +5,7 @@ public:
       unordered_map<int, int> m{{0, -1}}; //{sum, index}
       
       for(int i=0; i<nums.size(); i++){
-        sum += nums[i];
+        sum += (nums[i])?1:-1;
         if(m.count(sum)) res=max(res, i - m[sum]);
         else m[sum] = i;
       }
