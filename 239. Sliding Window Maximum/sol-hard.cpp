eef89400fdb17dc05308b73c1d9e9445cb379849
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-02-28 15:43:55 +0800

    Update sol-hard.cpp

--- 239. Sliding Window Maximum/sol-hard.cpp
+++ 239. Sliding Window Maximum/sol-hard.cpp
@@ -1,3 +1,4 @@
+// heap O(nlogn)
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -8,6 +9,23 @@ public:
             while(!q.empty() && i - q.top().second >= k) q.pop(); 
             q.push({nums[i], i});
             if(i >= k-1) res.push_back(q.top().first);
+        }
+        return res;
+    }
+};
+
+// dequeue O(n)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        deque<int> q;
+
+        for(int i=0; i<nums.size(); i++){
+            if(!q.empty() && q.front() == i-k) q.pop_front();
+            while(!q.empty() && nums[q.back()] < nums[i]) q.pop_back(); // previous less than i can remove, because the max must be i
+            q.push_back(i);
+            if(i >= k-1) res.push_back(nums[q.front()]);
         }
         return res;
     }
