da988b662e31c69c3a1c24ca15bb02790b79180d
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-01 13:31:07 +0800

    Update sol-hard.cpp

--- 123. Best Time to Buy and Sell Stock III/sol-hard.cpp
+++ 123. Best Time to Buy and Sell Stock III/sol-hard.cpp
@@ -1,3 +1,5 @@
+
+// 2D DP
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -16,5 +18,58 @@ public:
             }
         }
         return g[n-1][2];
+    }
+};
+
+// 1D DP
+// Because updating a state for a larger j (number of transactions) relies on the data from i-1 days.
+// If j starts from a smaller number, it might be overwritten by the state of i, j, not i-1, j.
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        //  j transactions over the first i days and not holding any stock on the i-th day.
+        vector<int> g(3); 
+
+        //  j transactions over the first i days and holding any stock on the i-th day.
+        vector<int> l(3); 
+
+        for(int i=1; i<n; i++){
+            int diff = prices[i] - prices[i-1];
+            for(int j=2; j>=1; j--){
+                l[j] = max(g[j-1] + max(diff,0), l[j]+diff);
+                g[j] = max(g[j], l[j]);
+            }
+        }
+        return g[2];
+    }
+};
+
+// 2 transactions
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> leftp(n), rightp(n);
+        int minp = prices[0], maxp = prices[n-1];
+
+        // first one transaction before i day
+        for(int i=1; i<n; i++){
+            leftp[i] = max(leftp[i-1], prices[i] - minp);
+            minp = min(minp, prices[i]);
+        }
+
+        // second one transaction after i day
+        for(int i=n-2; i>=0; i--){
+            rightp[i] = max(rightp[i+1], maxp - prices[i]);
+            maxp = max(maxp, prices[i]);
+        }
+
+        int res = 0;
+        // combine
+        for(int i=1; i<n; i++){
+            res = max(res, leftp[i] + rightp[i]);
+        }
+        return res;
     }
 };
