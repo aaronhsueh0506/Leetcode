0b5d56eb8b4be4c87bd076913815fa3355135d5a
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-02 09:16:57 +0800

    Create sol-medium.cpp

---
+++ 1136. Parallel Courese/sol-medium.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumSemesters(int n, vector<vector<int>>& relations) {
+      vector<vector<int>> graph(n+1);
+      vector<int> indeg(n+1); // number of prev classes for i class
+
+      for(auto a: relations){
+        graph[a[0]].push_back(a[1]);
+        indeg[a[1]]++;
+      }
+
+      queue<int> q;
+      for(int i=1; i<=n; i++){
+        if(indeg[i]==0) q.push(i);
+      }
+
+      int sem = 0, cnt = 0;
+
+      while(!q.empty()){
+        int s = q.size();
+        while(s--){
+          int cur = q.front(); q.pop();
+          cnt++; 
+          for(int next: graph[cur]){
+            if(--indeg[next]==0) q.push(next); 
+          }
+        }
+        sem++;
+      }
+      return cnt == n ? sem : -1;
+    }
+};
