2aed7fac31195abc1647659595f439ac3507f965
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-03 16:12:25 +0800

    Create sol-medium.cpp

---
+++ 155. Min Stack/sol-medium.cpp
@@ -0,0 +1,26 @@
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s1.push(val);
+        if(s2.empty() || val<=s2.top()) s2.push(val);
+    }
+    
+    void pop() {
+        if(s1.top()==s2.top()) s2.pop();
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+private: 
+    stack<int> s1, s2;
+};
