2471b526027387eaff2562e503bbb40864e75b0c
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-03 17:30:39 +0800

    Create sol-medium.cpp

---
+++ 235. Lowest Common Ancestor of a Binary Search Tree/sol-medium.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return nullptr;
+        if (root->val > max(p->val, q->val)){
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        else if(root->val < min(p->val, q->val)){
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        else return root;
+    }
+};
