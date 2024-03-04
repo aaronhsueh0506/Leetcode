53a82c6909828922e021947302c4955d40ea49ff
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-04 21:41:51 +0800

    Update sol-medium.cpp

--- 253. Meeting Rooms II/sol-medium.cpp
+++ 253. Meeting Rooms II/sol-medium.cpp
@@ -4,10 +4,9 @@ public:
         sort(intervals.begin(), intervals.end());
 
         priority_queue<int, vector<int>, greater<>> pq;
-        pq.push(intervals[0][1]);
 
-        for(int i=1; i<intervals.size(); i++){
-            if(pq.top() <= intervals[i][0]) pq.pop();
+        for(int i=0; i<intervals.size(); i++){
+            if(!pq.empty() && pq.top() <= intervals[i][0]) pq.pop();
             pq.push(intervals[i][1]);
         }
         return q.size();
