c8e95422ad87fa6a2b2f2654cc3ed11222a35b89
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-06 11:03:03 +0800

    Create sol-medium.cpp

---
+++ 490. The Maze/sol-medium.cpp
@@ -0,0 +1,55 @@
+// for each cell, the complexity is max(m,n)^4, so total is O(mn * max(m,n)^4)
+// DFS
+class Solution {
+public:
+    vector<vector<int>> dirs{{0,-1},{0,1},{-1,0},{1,0}};
+    int m, n;
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        m = maze.size(), n = maze[0].size();
+        return DFS(maze, start[0], start[1], destination[0], destination[1]);
+    }
+
+    bool DFS(vector<vector<int>>& maze, int x, int y, int dx, int dy){
+        if(x==dx && y==dy) return true;
+        maze[x][y]=-1;
+        bool res=false;
+        for(auto dir: dirs){
+            int nx = x, ny = y;
+            while(nx>=0 && ny>=0 && nx<m && ny<n && maze[nx][ny]!=1){ // find the first invaild position
+                nx+=dir[0]; ny+=dir[1];
+            }
+            nx -= dir[0]; ny-=dir[1]; // back to valid position
+            if(maze[nx][ny]!=-1) res |= DFS(maze, nx, ny, dx, dy);
+        }
+        return res;
+    }
+};
+
+// BFS
+class Solution {
+public:
+    vector<vector<int>> dirs{{0,-1},{0,1},{-1,0},{1,0}};
+    int m, n;
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        m = maze.size(), n = maze[0].size();
+        queue<pair<int,int>> q;
+        q.push({start[0], start[1]});
+        vector<vector<bool>> visited(m, vector<bool>(n));
+
+        while(!q.empty()){
+            auto t = q.front(); q.pop();
+            if(t.first == destination[0] && t.second == destination[1]) return true;
+            for(auto dir: dirs){
+                int nx = t.first, ny = t.second;
+                while(nx>=0 && ny>=0 && nx<m && ny<n && maze[nx][ny]!=1){
+                    nx += dir[0]; ny+= dir[1];
+                }
+                nx-=dir[0]; ny-=dir[1];
+                if(visited[nx][ny]) continue;
+                visited[nx][ny]=true;
+                q.push({nx,ny});
+            }
+        }
+        return false;
+    }
+};
