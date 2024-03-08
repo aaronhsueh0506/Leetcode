ac464eb8f39f3b6b5d59ceebf090c8d5d323f299
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-08 09:42:45 +0800

    Update sol-medium.cpp

--- 362. Design Hit Counter/sol-medium.cpp
+++ 362. Design Hit Counter/sol-medium.cpp
@@ -17,3 +17,32 @@ public:
 private:
     queue<int> q;
 };
+
+// follow-up
+class HitCounter {
+public:
+    HitCounter() {
+        times.resize(300);
+        hits.resize(300);
+    }
+    
+    void hit(int timestamp) {
+        if(times[timestamp%300] != timestamp){
+            times[timestamp%300] = timestamp;
+            hits[timestamp%300] = 1;
+        }
+        else{
+            hits[timestamp%300]++;
+        }
+    }
+    
+    int getHits(int timestamp) {
+        int res = 0;
+        for(int i=0; i<300; i++){
+            if(timestamp - times[i] < 300) res += hits[i];
+        }
+        return res;
+    }
+private:
+    vector<int> times, hits;
+};
