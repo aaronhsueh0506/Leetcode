dbd451745949afdec5aa38b79ced1ef894234a48
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-14 13:55:46 +0800

    Create sol-medium.cpp

---
+++ 1249. Minimum Remove to Make Valid Parentheses/sol-medium.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        string res;
+        stack<int> st;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='(') st.push(i);
+            else if(s[i]==')'){
+                if(st.empty()) s[i] = '*';
+                else st.pop();
+            }
+        }
+
+        while(!st.empty()){
+            s[st.top()] = '*'; st.pop();
+        }
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i]!='*') res += s[i];
+        }
+        return res;
+    }
+};
