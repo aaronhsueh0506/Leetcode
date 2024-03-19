423bec7f428254eb89a31cd71751a81b7974cdf8
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-19 11:14:01 +0800

    Update sol-hard.cpp

--- 4. Median of Two Sorted Arrays/sol-hard.cpp
+++ 4. Median of Two Sorted Arrays/sol-hard.cpp
@@ -3,7 +3,7 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
       
-        if(n1>n2) findMedianSortedArrays(nums2, nums1);
+        if(n1>n2) return findMedianSortedArrays(nums2, nums1);
 
         int k = (n1+n2+1)/2; // m1 + m2 = k = (n1+n2+1) / 2
         
@@ -11,16 +11,16 @@ public:
         while(left<right){
           int m1 = left + (right - left) / 2;
           int m2 = k - m1;
-          if(nums1[m1] < nums2[m2-1]) left = mid+1; // ideal is nums1[m1-1] and nums2[m2-1], so check nums1[m1] < nums[m2-1], m1 need explore right side
-          else right = mid;
+          if(nums1[m1] < nums2[m2-1]) left = m1+1; // ideal is nums1[m1-1] and nums2[m2-1], so check nums1[m1] < nums[m2-1], m1 need explore right side
+          else right = m1;
         }
         // binary search will stop at nums1[m1] >= nums2[m2-1] 
         
         int m1 = left, m2 = k - left;
-        int c1 = max(m1==0? INT_MIN : nums[m1-1], m2==0? INT_MIN: nums[m2-1]);
+        int c1 = max(m1==0? INT_MIN : nums1[m1-1], m2==0? INT_MIN: nums2[m2-1]);
         if( (n1+n2) % 2 ) return c1;
 
-        int c2 = min(m1==n1 ? INT_MAX: nums[m1], m2==n2? INT_MAX: nums[m2]);
+        int c2 = min(m1==n1 ? INT_MAX: nums1[m1], m2==n2? INT_MAX: nums2[m2]);
         return (c1+c2)/2;
     }
 };
