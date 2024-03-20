d637718297f16aa0538af52d657d17bd7ef86c88
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-20 10:52:09 +0800

    Update sol-hard.cpp

--- 124. Binary Tree Maximum Path Sum/sol-hard.cpp
+++ 124. Binary Tree Maximum Path Sum/sol-hard.cpp
@@ -8,8 +8,8 @@ public:
 
     int traversal(TreeNode* root, int& res){
       if(!root) return 0;
-      int left = traversal(root->left, res);
-      int right = traversal(root->right, res);
+      int left = max(traversal(root->left, res),0);
+      int right = max(traversal(root->right, res),0);
       res = max(res, left+right+root->val); // subtree, without parent
       return max(left,right) + root->val; // give bigger path to parent (left or right)
     }
