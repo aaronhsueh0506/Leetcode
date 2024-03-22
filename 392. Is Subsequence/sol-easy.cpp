ea8230c37e3383cf684fb45186997b4f47807047
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-22 13:40:38 +0800

    Update sol-easy.cpp

--- 392. Is Subsequence/sol-easy.cpp
+++ 392. Is Subsequence/sol-easy.cpp
@@ -11,3 +11,16 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int pos = 0;
+        for(int i=0; i<t.size(); i++){
+            if(t[i]==s[pos]){
+                pos++;
+            }
+        }
+        return pos == s.size();
+    }
+};
