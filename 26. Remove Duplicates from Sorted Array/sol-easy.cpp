ed3220b89ee029f3b36821e94d74e7c00d1ebb0e
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-24 14:15:23 +0800

    Create sol-easy.cpp

---
+++ 26. Remove Duplicates from Sorted Array/sol-easy.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=nums[j]){
+                nums[++j] = nums[i];
+            }
+        }
+        return j+1;
+    }
+};
