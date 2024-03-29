70ee13686f373bea0232e3e3394ed6e23323f590
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-29 13:33:39 +0800

    Create sol-medium.cpp

---
+++ 117. Populating Next Right Pointers in Each Node II/sol-medium.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return nullptr;
+        queue<Node*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int s = q.size();
+            for(int i=0; i<s; i++){
+                Node *t = q.front(); q.pop();
+                if(i<s-1) t->next = q.front();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+        }
+        return root;
+    }
+};
