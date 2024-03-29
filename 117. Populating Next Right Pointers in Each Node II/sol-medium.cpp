6b76597e686212bfb13ce9ecb3375b9d5c6a0d63
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-03-29 13:38:51 +0800

    Update sol-medium.cpp

--- 117. Populating Next Right Pointers in Each Node II/sol-medium.cpp
+++ 117. Populating Next Right Pointers in Each Node II/sol-medium.cpp
@@ -19,6 +19,33 @@ public:
 class Solution {
 public:
     Node* connect(Node* root) {
+        Node* dummy = new Node(-1), *cur = dummy;
+        Node* head =  root;
+
+        while(root){
+            if(root->left){
+                cur->next = root->left;
+                cur = cur->next;
+            }
+            if(root->right){
+                cur->next = root->right;
+                cur = cur->next;
+            }
+            root = root->next;
+
+            if(!root){
+                cur = dummy;
+                root = dummy->next;
+                dummy->next = nullptr;
+            }
+        }
+        return head;
+    }
+};
+
+class Solution {
+public:
+    Node* connect(Node* root) {
         if(!root) return nullptr;
         queue<Node*> q;
         q.push(root);
