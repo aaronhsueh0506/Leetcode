3266db7e58e7a3845c566d79805c977339257d95
Arron Hsueh <58929071+aaronhsueh0506@users.noreply.github.com>
2024-04-16 16:02:20 +0800

    Create sol-medium.cpp

---
+++ 1020. Number of Enclaves/sol-medium.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int m,n;
+    int numEnclaves(vector<vector<int>>& grid) {
+        int res = 0;
+        m = grid.size();
+        n = grid[0].size();
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || j==0 || i==m-1 || j==n-1) dfs(grid, i, j);
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1) res++;
+            }
+        }
+        return res;
+    }
+
+    void dfs(vector<vector<int>>& grid, int i, int j){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!=1) return;
+        grid[i][j]=0;
+        dfs(grid, i-1, j);    
+        dfs(grid, i, j-1);    
+        dfs(grid, i+1, j);    
+        dfs(grid, i, j+1);    
+    }
+};
